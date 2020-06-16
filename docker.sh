d6631ffee7434452c3822c362f4eedee7aa10856
Patrick Musau <pmusau13ster@gmail.com>
2020-06-16 08:48:15 -0500

    minor change to docker.sh

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --name=f1tenth_gym_container --rm --net=host f1tenth_gym
+docker run -it --name=f1tenth_gym_container -v $(pwd)/waypoints:/catkin_ws/src/f1tenth_gym_ros/waypoints --rm --net=host f1tenth_gym
