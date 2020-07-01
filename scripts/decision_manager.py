ec0c7380bcddce54894be80b57c7a401e8b37058
Patrick Musau <pmusau13ster@gmail.com>
2020-07-01 13:08:24 -0500

    added decision manager, very jerky

--- scripts/decision_manager.py
+++ scripts/decision_manager.py
@@ -7,11 +7,12 @@ from nav_msgs.msg import Odometry
 import numpy as np
 
 class Decision_Manager:
-    def __init__(self,safety_topic,drive_topic):
+    def __init__(self,safety_topic,drive_topic,drive_pub):
 
         # Initialize subscribers
         self.scan_subscriber=Subscriber(safety_topic,StampedBool,queue_size=100)
         self.odom_subscriber=Subscriber(drive_topic,AckermannDriveStamped,queue_size=100)
+        self.drive_publisher=rospy.Publisher(drive_pub,AckermannDriveStamped,queue_size=10)
 
         #create the time synchronizer
         self.sub = ApproximateTimeSynchronizer([self.scan_subscriber,self.odom_subscriber], queue_size = 100, slop = 0.020)
@@ -20,8 +21,11 @@ class Decision_Manager:
 
     
     def master_callback(self,bool_topic,drive_topic):
-        print(bool_topic)
-        print(drive_topic)
+        msg = drive_topic
+        if(bool(bool_topic.data)):
+            msg.drive.speed=0        
+        self.drive_publisher.publish(msg)
+
 
 
 
@@ -32,12 +36,13 @@ if __name__ == "__main__":
     args = rospy.myargv()[1:]
     bool_topic=args[0]
     drive_topic=args[1]
+    drive_pub = args[2]
 
     # start the node
 
     rospy.init_node('decision manager')
     
     #initialize the decision manager
-    dm = Decision_Manager(bool_topic,drive_topic)
+    dm = Decision_Manager(bool_topic,drive_topic,drive_pub)
 
     rospy.spin()
